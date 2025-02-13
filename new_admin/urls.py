b5466e63e627f813fc2ebe1875bc0ae7028381d2
Andrew Dzobko <mytyber@gmail.com>
2025-02-12 22:40:25 -0800

    Add new templates and styles for admin dashboard and store branding

--- new_admin/urls.py
+++ new_admin/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("dashboard", views.dashboard, name="dashboard"),
+    path("dashboard", views.dashboard, name="admin-dashboard"),
+    path("dashboard/home", views.dashboard_home, name="admin-dashboard-home"),
+    
+    path("store", views.store, name="admin-store"),
+    path("store/branding", views.store_branding, name="admin-store-branding"),
 ]
--- new_admin/views.py
+++ new_admin/views.py
@@ -6,7 +6,16 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    return redirect(reverse("dashboard"))
+    return redirect(reverse("admin-dashboard-home"))
 
 def dashboard(request):
-    return render(request, "new_admin/dashboard.html")
+    return redirect(reverse("admin-dashboard-home"))
+
+def dashboard_home(request):
+    return render(request, "new_admin/dashboard_home.html")
+
+def store(request):
+    return redirect(reverse("admin-store-branding"))
+
+def store_branding(request):
+    return render(request, "new_admin/store_branding.html")
