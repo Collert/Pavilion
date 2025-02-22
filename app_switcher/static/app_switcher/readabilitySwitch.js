477b2a5cd54482c3e58044686c703a0fcc827018
Andrew Dzobko <mytyber@gmail.com>
2025-02-21 19:46:28 -0800

    Update readabilitySwitch.js

--- app_switcher/static/app_switcher/readabilitySwitch.js
+++ app_switcher/static/app_switcher/readabilitySwitch.js
@@ -1,3 +1,7 @@
-if (new URLSearchParams(window.location.search).get("force-off-readability")) {
+const READABILITY_DISABLED = window.sessionStorage.getItem("force-off-readability") === "true" || new URLSearchParams(window.location.search).get("force-off-readability") === "true";
+if (READABILITY_DISABLED) {
     document.documentElement.style.colorScheme = "only light";
+    if (window.sessionStorage.getItem("force-off-readability") !== "true") {
+        window.sessionStorage.setItem("force-off-readability", READABILITY_DISABLED)
+    }
 }
